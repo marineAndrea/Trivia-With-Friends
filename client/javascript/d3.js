b2db3064d4c3fed4b4fabbbbd5e227eb4eaf6a52
eliotwinder <musicismymistress@gmail.com>
2015-08-20 16:11:46 -0700

    d3active

--- client/javascript/d3.js
+++ client/javascript/d3.js
@@ -4,37 +4,41 @@ angular.module('app.d3', [])
   var height = 300;
   var color = 'purple';
 
+  console.log('link function', scope.d3values);
   return {
-    restrict: 'E',
+    restrict: 'EA',
 
-    scope: false,
+    scope: {
+      d3values: 
+    },
 
     link: function (scope, element, attrs) {
-      var graph = d3.select(element[0]);
-      
-      var updateGraph = function(){
-        graph.selectAll('*').remove();
-        var svg = graph.append('svg')
-            .attr('width', width)
-            .attr('height', height);
-          
-        svg.append("rect")
-          .attr("width", "100%")
-          .attr("height", "100%")
-          .attr("fill", "purple");
+      scope.
+        console.log(element);
+        var graph = d3.select(element[0]);
+        
+        var updateGraph = function(newValue){
+          graph.selectAll('*').remove();
+          var svg = graph.append('svg')
+              .attr('width', width)
+              .attr('height', height);
+            
+          svg.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("fill", "purple");
 
-        svg.selectAll('p').data(scope.d3values)
-          .enter().append('rect')
-          .attr('fill', 'yellow')
-          .attr('width', '10px')
-          .attr('height', '10px');
-
-
-      };
-      
-      scope.$watch('d3values', function (newVal, oldVal) {
-        updateGraph();
-      });
+          svg.selectAll('p').data(scope.d3values)
+            .enter().append('rect')
+            .attr('fill', 'yellow')
+            .attr('width', '10px')
+            .attr('height', '10px');
+        };    
+        
+        scope.$watch('d3values', function (newVal, oldVal) {
+          updateGraph(scope.d3values);
+          console.log('hello');
+        });
     }
   };
 });
--- client/javascript/stats.js
+++ client/javascript/stats.js
@@ -4,9 +4,48 @@
   var app = angular.module('Stats',[]);
 
   app.controller('StatsController', ['$scope', function($scope) {
-
     //nothing for now. Will add later.
-
+    $scope.d3values = [1,2,3,4,5,6,7,8];
+    $scope.d3values.push(8);
   }]);
 
+  app.directive('dthree', function(){
+  var width = 300;
+  var height = 300;
+  var color = 'purple';
+  
+  return {
+    restrict: 'EA',
+
+    scope: 
+
+    link: function (scope, element, attrs) {
+        console.log(element);
+        var graph = d3.select(element[0]);
+        
+        var updateGraph = function(newValue){
+          graph.selectAll('*').remove();
+          var svg = graph.append('svg')
+              .attr('width', width)
+              .attr('height', height);
+            
+          svg.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("fill", "purple");
+
+          svg.selectAll('p').data(scope.d3values)
+            .enter().append('rect')
+            .attr('fill', 'yellow')
+            .attr('width', '10px')
+            .attr('height', '10px');
+        };    
+        
+        scope.$watch('d3values', function (newVal, oldVal) {
+          updateGraph(scope.d3values);
+          console.log('hello');
+        });
+    }
+  };
+});
 })();
