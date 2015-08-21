b06dec7f2aa7a83b215cebcc41ba8a887af64614
Marine Guillanton <marine.guillanton@gmail.com>
2015-08-21 10:31:28 -0700

    d3 graph

--- d3/test.js
+++ d3/test.js
@@ -1,33 +1,130 @@
-var widthGenerator = function(min, max) {
-  var res = Math.random() * (max - min) + min;
-  console.log('res', res);
+
+/////////////////////////////////////////* INPUT */////////////////////////////////////////
+
+var opponentsArr = [
+  {name: 'a', level: 15, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 13, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 10, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 12, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 10, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 10, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 11, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+  {name: 'a', level: 10, ngp: 0},
+  {name: 'b', level: 5, ngp: 5},
+  {name: 'c', level: 4, ngp: 10},
+  {name: 'd', level: 8, ngp: 15},
+];
+
+// a     b     c       d       e 
+// red   blue  yellow  violet  green 
+ 
+/////////////////////////////////////////* SETTINGS */////////////////////////////////////////
+
+var w = 600;
+var h = w/2;
+
+/////////////////////////////////////////* OUTPUT CREATION */////////////////////////////////////////
+
+var findMaxNgp = function(arr) {
+  var max = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].ngp > max) {
+      max = arr[i].ngp;
+    }
+  }
+  return max;
+};
+
+var completeTheYs = function(arr, h) {
+  var maxNgp = findMaxNgp(arr);
+  for (var i = 0; i < arr.length; i++) {
+    var y = maxNgp - arr[i].ngp;
+    var propY = Math.floor((y * h) / (maxNgp));
+    arr[i].y = propY;
+  }
+  return arr;
+};
+
+var xGenerator = function(min, max) {
+  var res = Math.random() * ((max - min)/10) + (min/10);
+  res = Math.floor(res);
+  res = res * 10;
   return res;
 };
 
-console.log('random', widthGenerator(20, 780));
+var findX = function(w, y) {
+  var min = (w / 2) - y;
+  var max = (w / 2) + y;
+  return xGenerator(min, max);
+};
 
-var w = 800;
-var h = 240;
+var getDiff = function(w, x) {
+  var m = w/2;
+  var diff;
+  if (x >= m) {
+    diff = x - m;
+  } else {
+    diff = m - x;
+  }
+  return diff;
+};
+
+var getSin = function(h, d) { // hypothenuse, diff
+  var res = (h*h - d*d);
+  res = Math.sqrt(res);
+  return res;
+};
+
+var findY = function(w, x, y) {
+  var d = getDiff(w, x);
+  var res = Math.round(getSin(y, d));
+  return res;
+};
+
+var CompleteXandY = function(arr, w, h) {
+  // get the ys
+  var output = completeTheYs(arr, h);
+  for (var i = 0; i < output.length; i++) {
+    var y = output[i].y;
+    var x = findX(w, y);
+    // find x given y
+    // given x find new y (or sin)
+      // given x find diff
+      // given diff find sin
+    y = findY(w, x, y);
+    output[i].x = x;
+    output[i].y = y;
+  }
+  return output;
+};
+
+var output = CompleteXandY(opponentsArr, w, h);
+console.log('output', output);
+
+/////////////////////////////////////////* D3 VISUALIZATION */////////////////////////////////////////
+
 var maxRadius = 10;
- // a     b     c       d       e 
- // red   blue  yellow  violet  green 
- // a     e     b       d       c
- // red   green blue    violet  yellow
-/*var opponentsArr = [
-  {name: 'a', score: 10, ngp: 0},
-  {name: 'b', score: 5, ngp: 2},
-  {name: 'c', score: 15, ngp: 4},
-  {name: 'd', score: 20, ngp: 3},
-  {name: 'e', score: 15, ngp: 1},
-];*/
-
-var opponentsArr = [
-  {name: 'a', score: 10, ngp: 10},
-  {name: 'b', score: 5, ngp: 8},
-  {name: 'c', score: 15, ngp: 5},
-  {name: 'd', score: 20, ngp: 0},
-  {name: 'e', score: 15, ngp: 7},
-];
 
 svgContainer = d3.select('body')
   .append('svg')
@@ -35,49 +132,49 @@ svgContainer = d3.select('body')
   .attr('width', w)
   .classed('opponents', true);
 
-maxOppRadius = d3.max(opponentsArr, function(opponent) {
-  console.log('opponent.score/2', opponent.score/2);
-  return opponent.score/2;
+maxOppRadius = d3.max(output, function(opponent) {
+  return opponent.level;
 });
 
 oppRadiusScale = d3.scale.linear()
   .domain([0, maxOppRadius])
   .range([0, maxRadius]);
 
-
-maxNgp = d3.max(opponentsArr, function(opponent) {
-  console.log('opponent.ngp', opponent.ngp);
-  return opponent.ngp;
+maxY = d3.max(output, function(opponent) {
+  return opponent.y;
+});
+maxX = d3.max(output, function(opponent) {
+  return opponent.x;
 });
 
 heightScale = d3.scale.linear()
-  .domain([0, maxNgp])
+  .domain([0, maxY])
   .range([maxRadius*2, h - maxRadius*2]);
+widthScale = d3.scale.linear()
+  .domain([0, maxX])
+  .range([maxRadius*2, w - maxRadius*2]);
+
 
 opponents = svgContainer.selectAll('circle.opponent')
-  .data(opponentsArr)
+  .data(output)
   .enter()
   .append('circle')
-  .attr('cx', w/2) 
-  // .attr('cx', widthGenerator(20, 780))
+  .attr('cx', w/2)
+  .attr('cy', heightScale(h))
   .attr('r', function(opponent) {
-    return oppRadiusScale(opponent.score/2);
+    return oppRadiusScale(opponent.level);
   })
   .attr('class', function(opponent) {
     return opponent.name;
-  })
-  .attr('cy', h - maxRadius*2);
+  });
 
 opponents
   .transition()
   .delay(1000)
-  .attr('cy', function(opponent, i) {
-    return heightScale(opponent.ngp);
-  })
-  // get random value between maxRadius*2 and w - maxRadius*2
   .attr('cx', function(opponent, i) {
-    return widthGenerator(20, 780);
+    return widthScale(opponent.x);
+  })
+  .attr('cy', function(opponent, i) {
+    return heightScale(h - opponent.y);
   });
-
-
 
